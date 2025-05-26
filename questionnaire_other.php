82dc79b98240c8fec8ff348a4d24c6e8e18ca48b
Zmey25 <zmeyker25@gmail.com>
2025-05-27 01:58:13 +0300

    Update questionnaire_other.php

--- questionnaire_other.php
+++ questionnaire_other.php
@@ -1,7 +1,4 @@
 <?php // questionnaire_other.php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/questionnaire_logic.php';
 require_once __DIR__ . '/includes/trait_calculator.php';
